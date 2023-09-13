5bdfd1d0ae4c3ba7b998db100c3cc0fd750629ea
Sydney Nguyen <sydney2004nguyen@gmail.com>
2023-09-13 19:01:20 -0400

    Test 2 Mobile Scroll

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -24,7 +24,7 @@ export const Nav = (props) => {
           />
         </button>
         <div
-          className={`z-10 fixed top-0 right-0 bottom-0 bg-pink-50 transition-all flex flex-col
+          className={`z-10 fixed top-0 right-0 bottom-0 bg-pink-50 transition-all overflow-hidden flex flex-col
         ${menuOpened ? "w-80" : "w-0"}`}
         >
           <div className="flex-1 flex items-start justify-center flex-col gap-6 p-8">
